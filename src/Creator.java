406d0f95fc8fe2a64a00c5e0356a22bf19320bf5
April Ross <aprilross@aprils-mbp.lan>
2017-07-28 15:16:39 -0400

    added in jackson library

--- src/Creator.java
+++ src/Creator.java
@@ -1,9 +1,34 @@
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by David Turk on 7/28/17.
  */
 public class Creator {
     public void createWorkOrders() {
-        // read input, create work orders and write as json files
+        // read input:
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the WorkOrder description:”);
+                creator.setDescription(scanner.nextline());
+
+        System.out.println("Enter the name of who sent the WorkOrder:");
+        creator.setSenderName(scanner.nextline());
+
+        creator.setStatus = Status.INITIAL;
+
+        creator.getId();
+        // create work orders
+
+        // write as json files
+
+        ObjectMapper mapper = new ObjectMapper();
+        fileName = creator.getId();
+        mapper.writeValue(new File(fileName), creator);
+        String json = mapper.writeValueAsString(creator);
+
     }
 
     public static void main(String args[]) {
