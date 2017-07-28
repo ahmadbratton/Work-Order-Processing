40f2a347a0533cab40c53001caeeb5fb46515533
DavidTurkGT <dlturk747@gmail.com>
2017-07-28 14:02:57 -0400

    Intial commit

---
+++ src/Creator.java
@@ -0,0 +1,13 @@
+/**
+ * Created by David Turk on 7/28/17.
+ */
+public class Creator {
+    public void createWorkOrders() {
+        // read input, create work orders and write as json files
+    }
+
+    public static void main(String args[]) {
+        Creator creator = new Creator();
+        creator.createWorkOrders();
+    }
+}
---
+++ src/Status.java
@@ -0,0 +1,5 @@
+/**
+ * Created by David Turk on 7/28/17.
+ */
+public enum Status {
+}
