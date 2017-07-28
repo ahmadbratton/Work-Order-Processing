328ec955891251efe8f9ff8e93b4ac6fb72518d5
Davis Uhlig <duhlig@daviss-mbp.lan>
2017-07-28 14:14:02 -0400

    added WorkOrder class

--- src/WorkOrder.java
+++ src/WorkOrder.java
@@ -2,4 +2,43 @@
  * Created by David Turk on 7/28/17.
  */
 public class WorkOrder {
+    private long id;
+    private String description;
+    private String senderName;
+    private Status status;
+
+    public WorkOrder(String description, String senderName, Status status) {
+        this.description = description;
+        this.senderName = senderName;
+        this.status = status;
+        id = System.currentTimeMillis();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
